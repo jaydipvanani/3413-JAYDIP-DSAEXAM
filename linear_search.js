eddd2f55ca1e8bdbece50c28fc614a7ab4c2220c
Vanani Jaydip <132327495+jaydipvanani@users.noreply.github.com>
2024-02-05 11:02:29 +0530

    Update linear_search.js

--- linear_search.js
+++ linear_search.js
@@ -1,18 +1,23 @@
 let arr = [12, 23, 45, 67, 11, 18, 90, 18, 23, 7, 52];
-let value = 18;
+let value = 52;
 
 
 function search(arr, value) {
 
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] == value) {
-            console.log(`${value} index ${i}`);
+            return i;
         }
-        else{
-             console.log("element is a not exist");
-        }
-
+        
     }
+    return false;
 
 }
-search(arr, value);
+let result = search(arr, value);
+
+if (result == false) {
+    console.log("Value is not in array");
+}
+else {
+        console.log("value is "+result);
+}
